4fb824123e7ca48d83d29edd59a360c7ac5a27e7
Oscar <oscarwebb162@protonmail.com>
2022-08-30 14:25:26 +1000

    Starting on file stuffs

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import requests
 from io import BytesIO
 import random
 import os
+from os.path import exists
+import pickle
 from dotenv import load_dotenv
 from tqdm import tqdm
 
@@ -13,16 +15,25 @@ load_dotenv('config.env')
 Bearer = 'Bearer ' + os.getenv('HEADER')
 headers = {'Authorization': Bearer}
 
-data = requests.get('https://api.github.com/repos/cat-milk/Anime-Girls-Holding-Programming-Books/contents/', headers=headers)
-ghJson = data.json()
 folders = []
 
-for x in tqdm(ghJson):
-    if not x['path'].__contains__("."):
-        data2 = requests.get(x['url'], headers=headers)
-        ghJson2 = data2.json()
-        for x2 in ghJson2:
-            folders.append(x2["download_url"])
+if exists('folders.txt'):
+    with open('folders.txt', 'rb') as f:
+        folders = pickle.load(f)
+else:
+    data = requests.get('https://api.github.com/repos/cat-milk/Anime-Girls-Holding-Programming-Books/contents/', headers=headers)
+    ghJson = data.json()
+
+    for x in tqdm(ghJson):
+        if not x['path'].__contains__("."):
+            data2 = requests.get(x['url'], headers=headers)
+            ghJson2 = data2.json()
+            for x2 in ghJson2:
+                folders.append(x2["download_url"])
+
+    file = open('folders.txt', 'x')
+    pickle.dump(str(folders), file)
+    file.close()
 
 url = folders[random.randint(0, len(folders)-1)]
 
